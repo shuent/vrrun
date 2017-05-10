a9889f5ab025fa799743748d4e8a201026614b8a
Shunta <shunshun.43@gmail.com>
2017-05-11 00:41:10 +0900

    create goal and game over in main, title scenes

---
+++ Assets/Scripts/GameClearScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameClearScript : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Player") {
+			Debug.Log ("player collided gameClear");
+			SceneManager.LoadScene ("gameClear");
+		}
+	}
+}
---
+++ Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScript : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Player") {
+			Debug.Log ("player collided gameover");
+			SceneManager.LoadScene ("gameOver");
+		}
+	}
+}
