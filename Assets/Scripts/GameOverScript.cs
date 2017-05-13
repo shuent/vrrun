7aa01d860030eb3361ed26ce463b272a5fcd3311
Shunta <shunshun.43@gmail.com>
2017-05-13 16:50:19 +0900

    one scene

--- Assets/Scripts/GameOverScript.cs
+++ Assets/Scripts/GameOverScript.cs
@@ -8,7 +8,14 @@ public class GameOverScript : MonoBehaviour {
 	void OnTriggerEnter(Collider other){
 		if (other.tag == "Player") {
 			Debug.Log ("player collided gameover");
-			SceneManager.LoadScene ("gameOver");
+			StopPlayer (other.gameObject);
+			// if Restart button
+			//     Timescale = 1;
 		}
+	}
+
+	void StopPlayer(GameObject player){
+		Time.timeScale = 0;
+		// player.GetComponent<Rigidbody> ().velocity = new Vector3(0, 0, 0);
 	}
 }
---
+++ Assets/Scripts/PlayerCanvasScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCanvasScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	// activated by GameCollider
+	void Update () {
+		FixPosition ();
+
+	}
+
+	void FixPosition(){
+		Vector3 pos = transform.position;
+		pos.x = -3 + Camera.main.transform.position.x;
+		pos.y = 0 + Camera.main.transform.position.y;
+		pos.z = 0 + Camera.main.transform.position.z;
+		transform.position = pos;
+	}
+}
