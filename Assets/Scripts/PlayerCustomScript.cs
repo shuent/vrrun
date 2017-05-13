444196633189d5a51093e256ebd1a19edcf3d515
Shunta <shunshun.43@gmail.com>
2017-05-13 09:48:58 +0900

    update

---
+++ Assets/Scripts/PlayerCustomScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCustomScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//RayHitAndDestroyEnemy ();
+		//Debug.Log (transform.position);
+
+	}
+
+	void RayHitAndDestroyEnemy(){
+		Ray downray = new Ray (transform.position - new Vector3(0,1,0), new Vector3(0,-2,0));
+		RaycastHit hit;
+		Debug.DrawRay (downray.origin, downray.direction, Color.red);
+
+		if (Physics.Raycast (downray, out hit, 1.5f)) {
+			if (hit.collider.tag == "Enemy") {
+				Debug.Log ("hit enemy");
+				Destroy (hit.collider.gameObject);
+			}
+		}
+	}
+
+
+
+}
---
+++ Assets/Scripts/isSteppedCollider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class isSteppedCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.tag == "Player") {
+			Debug.Log ("this is stepped1");
+			var _parent = transform.root.gameObject;
+			Destroy (_parent);
+		}
+	}
+}
